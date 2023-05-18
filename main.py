5d1b2b6cc3b3752d43f44a6b62c43797c00f7ce4
Alexey <alexcheremisin@mail.ru>
2023-05-18 21:21:47 +0300

    lesson_1

--- main.py
+++ main.py
@@ -1,1 +1,56 @@
-input()
+# print('Enter the first number')
+# a = input()
+# b = input('Enter the second number: ')
+# print(a*b)
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# print('Enter the first number: ')
+# a = int(input())
+# b = int(input('Enter the second number: '))
+# print(a*b)
+
+# a = 5.3444
+# b = 4.443
+# print(round(a*b, 3))
+
+# iter = 2
+# iter += 3
+# iter -= 4
+# iter *= 5
+# iter /= 5
+# iter //= 5
+# iter %= 5
+# iter **= 5
+
+# a = 1 > 4
+# print(a)
+# a = 1 < 4 and 5 > 2
+# print(a)
+# a = 1 == 2
+# print(a)
+# a = 1 != 2
+# print(a)
+# a = 'qwe'
+# b = 'qwe'
+# print (a==b)
+
+# a = 1 < 3 < 5 < 10
+# print(a)
+
+# a = 'leha'
+# for i in a:
+#     print(i)
+
+# text = 'cHo tI'
+# print(len(text))
+# print('nicho' in text)
+# print(text.lower())
+# print(text.upper())
+# print(text.replace('tI', 'NICHO'))
+
